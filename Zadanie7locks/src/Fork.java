20e732251fff4a123da80f0701aa4ce72b32c21b
Michał Fraś <michal@frasiek.com>
2014-12-14 08:56:05 +0100

    zadanie 7 na locku zamiast na synchronized

--- Zadanie7locks/src/Fork.java
+++ Zadanie7locks/src/Fork.java
@@ -1,19 +1,36 @@
-
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Fork {
 	private boolean busy = false;
+	private ReentrantLock lock = new ReentrantLock();
+	private Condition cnd;
 	
-	public synchronized void get(){
-		while(busy){
-			try {
-				wait();
-			} catch (InterruptedException e) {}
-		}
-		busy = true;
+	public Fork() {
+		cnd = lock.newCondition();
 	}
 	
-	public synchronized void put(){
-		busy = false;
-		notifyAll();
+	public void get(){
+		lock.lock();
+		try{
+			while(busy){
+				try {
+					cnd.await();
+				} catch (InterruptedException e) {}
+			}
+			busy = true;
+		} finally {
+			lock.unlock();
+		}
+	}
+	
+	public void put(){
+		lock.lock();
+		try{
+			busy = false;
+			cnd.signal();
+		} finally {
+			lock.unlock();
+		}	
 	}
 }
